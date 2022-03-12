e24046aa6903d4d0164b697d086db0cc47dfe125
João Vitor <joaovitor.novacruz@gmail.com>
2022-03-12 17:14:40 -0300

    Documentando o resource de Produto

--- src/main/java/com/gvendas/gestaovendas/resources/ProdutoResource.java
+++ src/main/java/com/gvendas/gestaovendas/resources/ProdutoResource.java
@@ -2,6 +2,8 @@ package com.gvendas.gestaovendas.resources;
 
 import com.gvendas.gestaovendas.models.Produto;
 import com.gvendas.gestaovendas.services.ProdutoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+@Api(tags = "Produto")
 @RestController
 @RequestMapping("produtos")
 public class ProdutoResource {
@@ -18,16 +21,20 @@ public class ProdutoResource {
     @Autowired
     private ProdutoService service;
 
+    @ApiOperation(value = "Buscar produto")
     @GetMapping(value = "{codigo}")
     public ResponseEntity<Produto> buscarProduto(@PathVariable Long codigo){
         return ResponseEntity.ok().body(service.buscarPorCodigo(codigo));
     }
 
+    @ApiOperation(value = "Listar produto")
     @GetMapping
     public ResponseEntity<List<Produto>> listaProduto() {
         return ResponseEntity.ok().body(service.listaProduto());
     }
 
+
+    @ApiOperation(value = "Salvar produto")
     @PostMapping
     public ResponseEntity<Produto> salvarProduto(@Valid @RequestBody Produto produto) {
         Produto produtoSalvo = service.salvarProduto(produto);
@@ -39,12 +46,14 @@ public class ProdutoResource {
         return ResponseEntity.created(uri).body(produtoSalvo);
     }
 
+    @ApiOperation(value = "Atualizar produto")
     @PutMapping(value = "{codigo}")
     public ResponseEntity<Void> atualizarProduto(@PathVariable Long codigo, @RequestBody Produto produto) {
         service.atualizarProduto(codigo, produto);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "Apagar produto")
     @DeleteMapping(value = "{codigo}")
     public ResponseEntity<Void> apagarProduto(@PathVariable Long codigo) {
         service.apagarProduto(codigo);
