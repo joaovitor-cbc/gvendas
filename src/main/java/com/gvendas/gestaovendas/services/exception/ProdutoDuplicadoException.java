1e5abda6c6df4b421440d2917b55672282360309
João Vitor <joaovitor.novacruz@gmail.com>
2022-03-12 17:07:33 -0300

    Adicionando excecao para produto duplicado

--- src/main/java/com/gvendas/gestaovendas/services/exception/ProdutoDuplicadoException.java
+++ src/main/java/com/gvendas/gestaovendas/services/exception/ProdutoDuplicadoException.java
@@ -1,6 +1,6 @@
 package com.gvendas.gestaovendas.services.exception;
 
-public class ProdutoDuplicadoException extends RuntimeException{
+public class ProdutoDuplicadoException extends RuntimeException {
 
     public ProdutoDuplicadoException(String message) {
         super(message);
